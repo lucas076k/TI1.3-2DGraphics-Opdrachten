d2a80f9769a73feee698570fdc69b544e34bd6ed
lucas076k <lwj.kuijsters@student.avans.nl>
2023-02-07 12:27:54 +0100

    2.3 Kleuren opdracht

--- Week2/003.Colors/src/Colors.java
+++ Week2/003.Colors/src/Colors.java
@@ -33,6 +33,59 @@ public class Colors extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        int x = 0;
+
+        for (int i = 0; i < 13; i++) {
+            Rectangle2D newRect = new Rectangle2D.Double(x, 216, 49, 49);
+
+            switch (i) {
+                case 0:
+                    graphics.setColor(Color.BLACK);
+                    break;
+                case 1:
+                    graphics.setColor(Color.BLUE);
+                    break;
+                case 2:
+                    graphics.setColor(Color.CYAN);
+                    break;
+                case 3:
+                    graphics.setColor(Color.DARK_GRAY);
+                    break;
+                case 4:
+                    graphics.setColor(Color.GRAY);
+                    break;
+                case 5:
+                    graphics.setColor(Color.GREEN);
+                    break;
+                case 6:
+                    graphics.setColor(Color.LIGHT_GRAY);
+                    break;
+                case 7:
+                    graphics.setColor(Color.MAGENTA);
+                    break;
+                case 8:
+                    graphics.setColor(Color.ORANGE);
+                    break;
+                case 9:
+                    graphics.setColor(Color.PINK);
+                    break;
+                case 10:
+                    graphics.setColor(Color.RED);
+                    break;
+                case 11:
+                    graphics.setColor(Color.WHITE);
+                    break;
+                case 12:
+                    graphics.setColor(Color.YELLOW);
+                    break;
+            }
+
+            graphics.fill(newRect);
+            graphics.draw(newRect);
+            x += 50;
+        }
+
     }
 
 
