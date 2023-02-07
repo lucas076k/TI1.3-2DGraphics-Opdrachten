a886dcf7145e6c0934b4f44e20cd468c9e962e15
lucas076k <lwj.kuijsters@student.avans.nl>
2023-02-07 12:06:14 +0100

    2.2 Yin-Yang opdracht

--- Week2/002.YingYang/src/YingYang.java
+++ Week2/002.YingYang/src/YingYang.java
@@ -33,6 +33,41 @@ public class YingYang extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        Ellipse2D bgBlack = new Ellipse2D.Double(100, 50, 400, 400);
+        graphics.setColor(Color.black);
+        graphics.fill(bgBlack);
+        graphics.draw(bgBlack);
+
+        Rectangle2D whiteRect = new Rectangle2D.Double(100, 50, 200, 500);
+        graphics.setColor(Color.WHITE);
+        graphics.fill(whiteRect);
+        graphics.draw(whiteRect);
+
+        Ellipse2D whiteUp = new Ellipse2D.Double(200, 50, 200, 200);
+        graphics.setColor(Color.white);
+        graphics.fill(whiteUp);
+        graphics.draw(whiteUp);
+
+        Ellipse2D downBlack = new Ellipse2D.Double(200, 253, 197, 197);
+        graphics.setColor(Color.black);
+        graphics.fill(downBlack);
+        graphics.draw(downBlack);
+
+        Ellipse2D bgOutline = new Ellipse2D.Double(100, 50, 400, 400);
+        graphics.setColor(Color.black);
+        graphics.setStroke(new BasicStroke(4)); //Changes (outer)line width
+        graphics.draw(bgOutline);
+
+        Ellipse2D blackDot = new Ellipse2D.Double(280, 130, 40, 40);
+        graphics.setColor(Color.black);
+        graphics.fill(blackDot);
+        graphics.draw(blackDot);
+
+        Ellipse2D whiteDot = new Ellipse2D.Double(280, 330, 40, 40);
+        graphics.setColor(Color.white);
+        graphics.fill(whiteDot);
+        graphics.draw(whiteDot);
     }
 
 
